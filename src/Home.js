0c60ea6099bc19dd05ab4ce2a9c03d5172d4585a
reemsellat <reemsellat@gmail.com>
2023-06-10 18:25:45 +0300

    first commit

--- src/Home.js
+++ src/Home.js
@@ -93,7 +93,7 @@ export default function Home(){
             <a href='#part-6'><span>04. </span>Contact</a>
   
           </nav>
-          <a href='./src/reem.pdf' target='_new'>
+          <a href='reemsellat.github.io/portfolio/src/reem.pdf' target='_new'>
           <div id='resume'>
              <button>Resume</button>
              </div>
